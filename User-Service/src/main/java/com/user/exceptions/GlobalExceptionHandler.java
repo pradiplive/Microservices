78a0849b1365b6e7b84ea68bceec4d5576998c9e
Pradip Laxman Sanap <pradips@cybage.com>
2025-07-25 14:12:47 +0530

    update files

--- User-Service/src/main/java/com/user/exceptions/GlobalExceptionHandler.java
+++ User-Service/src/main/java/com/user/exceptions/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
